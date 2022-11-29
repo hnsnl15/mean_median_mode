34febccb0b5a75b815233165f86b59c98bb5e52e
henson <hensonlao@gmail.com>
2022-11-29 20:53:26 +0800

    update.

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.get("/", (req, res) => {
+  res.status(200).json({message: "API is running."});
+});
+
 app.use("/", meanRoute);
 app.use("/", medianRoute);
 app.use("/", modeRoute);
