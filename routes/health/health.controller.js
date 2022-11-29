880ad279c19c568d7e2ecd71f570b52fb296579b
henson <hensonlao@gmail.com>
2022-11-29 20:13:52 +0800

    update.

--- routes/health/health.controller.js
+++ routes/health/health.controller.js
@@ -2,9 +2,9 @@ import asyncHandler from "express-async-handler";
 
 export const healthcheck = asyncHandler((req, res) => {
   try {
-    res.status(200);
+    res.status(200).json({message: "ok"});
   } catch (error) {
-    res.status(400);
+    res.status(400).json({message: "Error found!"});
   }
 
   return null;
